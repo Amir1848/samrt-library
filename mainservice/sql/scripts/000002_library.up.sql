4f9c267b307609a0a769fae7c4411bdfb57f08ba
amir.hesari1848@gmail.com <amir.hesari1848@gmail.com>
2023-12-12 22:16:43 +0330

    change

--- mainservice/sql/scripts/000002_library.up.sql
+++ mainservice/sql/scripts/000002_library.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE gnr_library (
     id BIGSERIAL PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
-    user_id BIGINT NOT NUL REFERENCES gnr_user(id),
+    user_id BIGINT NOT NULL REFERENCES gnr_user(id),
     token VARCHAR(50) NOT NULL
 );
