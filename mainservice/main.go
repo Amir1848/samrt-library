1dbd6d74ca977b01e62a8d6ad5454b8932eec474
Amir Hesari <amir.hesari1848@gmail.com>
2024-01-23 22:07:23 +0330

    socket testings

--- mainservice/main.go
+++ mainservice/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
 	"os"
+	"strings"
 
 	"github.com/Amir1848/samrt-library/routes"
 	"github.com/gin-gonic/gin"
@@ -34,5 +37,44 @@ func main() {
 
 	routes.AddMainRoutes(r)
 
+	go serveLibrarySockets()
 	r.Run()
+}
+
+func serveLibrarySockets() {
+	ln, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer func() {
+		fmt.Print("closing connection")
+		conn.Close()
+	}()
+
+	for {
+		a, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Printf("Received: %s", a)
+		if strings.EqualFold(a, "end\r\n") {
+			break
+		}
+	}
 }
